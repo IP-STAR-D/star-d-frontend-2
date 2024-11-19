baeb99882535c6d823e8199eaae16d78f1492272
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2024-11-19 22:05:40 +0200

    Add data

---
+++ src/app/data/degree.data.ts
@@ -0,0 +1,7 @@
+import { Degree } from '../models/degree.model';
+
+export const degreesData: Degree[] = [
+  new Degree(1, 'Computer Science', 1),
+  new Degree(2, 'Mechanical Engineering', 1),
+  new Degree(3, 'Electrical Engineering', 1),
+];
---
+++ src/app/data/professor.data.ts
@@ -0,0 +1,9 @@
+import { Professor } from '../models/professor.model';
+
+export const professorsData: Professor[] = [
+  new Professor(1, 1),
+  new Professor(2, 1),
+  new Professor(3, 1),
+  new Professor(4, 1),
+  new Professor(5, 1),
+];
