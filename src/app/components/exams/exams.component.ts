6c157af991cba3fefa5555d81bf479be5932ef33
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2024-11-19 20:02:05 +0200

    Integrate models in exams component

--- src/app/components/exams/exams.component.ts
+++ src/app/components/exams/exams.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { Exam } from '../../models/exam.model';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-exams',
@@ -12,18 +14,31 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './exams.component.css',
 })
 export class ExamsComponent {
-  exams = [
-    { id: '1', title: 'Ingineria programelor', professor: 'Turcu Cristina' },
-    { id: '2', title: 'Sisteme inteligente', professor: 'Turcu Corneliu' },
-    { id: '3', title: 'SIIEP', professor: 'Petrariu Adrian' },
-    { id: '4', title: 'Proiectarea bazelor de date', professor: 'Danubianu Mirela' },
-    { id: '5', title: 'Proiectarea translaatoarelor', professor: 'Gherman Ovidiu' },
-    { id: '6', title: 'Calcul mobil', professor: 'Gherman Ovidiu' },
+  exams: Exam[] = [
+    new Exam(1, 1, 'Ingineria programelor', 1, 2),
+    new Exam(2, 2, 'Sisteme inteligente', 1, 2),
+    new Exam(3, 3, 'SIIEP', 1, 2),
+    new Exam(4, 4, 'Proiectarea bazelor de date', 1, 2),
+    new Exam(5, 5, 'Proiectarea translatoarelor', 1, 2),
+    new Exam(6, 5, 'Calcul mobil', 1, 2),
+  ];
+
+  users: User[] = [
+    new User(1, 'turcu.cristina@usm.com', 'Cristina', 'Turcu', 'password123', false),
+    new User(2, 'turcu.corneliu@usm.com', 'Corneliu', 'Turcu', 'password123', false),
+    new User(3, 'petrariu.adrian@usm.com', 'Adrian', 'Petrariu', 'password123', false),
+    new User(4, 'danubianu.mirela@usm.com', 'Mirela', 'Danubianu', 'password123', false),
+    new User(5, 'gherman.ovidiu@usm.com', 'Ovidiu', 'Gherman', 'password123', false),
   ];
 
   constructor(private router: Router) {}
 
-  redirectToExam(examId: string): void {
+  redirectToExam(examId: number): void {
     this.router.navigate([`/exams/${examId}`]);
+  }
+
+  getProfessorName(professorId: number): string {
+    const professor = this.users.find((p) => p.userId === professorId);
+    return professor ? `${professor.firstName} ${professor.lastName}` : '';
   }
 }
