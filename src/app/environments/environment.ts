cdaf90ff3d80910be96c223f615b977c2f840ad4
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2024-12-18 03:00:23 +0200

    Fix environment files

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: process.env.API_URL || 'http://localhost:8081',
+  apiUrl: 'http://localhost:8081',
   firebaseConfig: {
-    apiKey: process.env.FIREBASE_API_KEY || 'default-api-key',
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'default-auth-domain',
-    projectId: process.env.FIREBASE_PROJECT_ID || 'default-project-id',
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'default-storage-bucket',
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || 'default-messaging-sender-id',
-    appId: process.env.FIREBASE_APP_ID || 'default-app-id',
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'default-measurement-id',
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+    projectId: process.env['FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_ID'],
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
   },
 };
