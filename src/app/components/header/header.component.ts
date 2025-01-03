29535c53e4252be752cfdbd86fde79cee9091ea7
Arekuso-kun <alexodrino@gmail.com>
2025-01-04 01:33:12 +0200

    Display user information in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
+import { SnackBarService } from '../../services/snack-bar.service';
+import { UserResponse } from '../../models/user.model';
+
+type UserRole = 'student' | 'professor' | 'admin';
 
 @Component({
   selector: 'app-header',
@@ -11,17 +16,54 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  logoutButton: boolean = true;
+  isNotLogin: boolean = true;
+  userInfo: UserResponse | undefined;
+  role: UserRole | undefined;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit(): void {
     this.router.events.subscribe(() => {
-      this.logoutButton = this.router.url !== '/login';
+      this.isNotLogin = this.router.url !== '/login';
+      if (this.authService.getToken()) {
+        this.loadUserData();
+      }
     });
   }
+
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  loadUserData(): void {
+    this.userService.getUserData().subscribe({
+      next: (data: UserResponse) => {
+        this.userInfo = data;
+        if (data.user.isAdmin) {
+          this.role = 'admin';
+        } else if (data.student) {
+          this.role = 'student';
+        } else if (data.professor) {
+          this.role = 'professor';
+        }
+
+        console.log('Datele utilizatorului:', this.userInfo);
+      },
+      error: (err) => {
+        console.error('Eroare la preluarea datelor utilizatorului:', err);
+        this.snackBarService.show('Eroare la preluarea datelor utilizatorului!', 'error');
+      },
+    });
+  }
+
+  acronym(str: string | undefined): string {
+    if (!str) return '';
+    return str.replace(/[^A-Z]/g, '');
   }
 }
