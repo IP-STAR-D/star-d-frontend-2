484682636d8c1480991e87f88882102733e9ec93
Arekuso-kun <alexodrino@gmail.com>
2025-01-03 02:39:21 +0200

    Clean up imports

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { RouterLink,Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  LogoutButton: boolean = true;
+  logoutButton: boolean = true;
 
-  constructor(private router:Router,private authService:AuthService){}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.router.events.subscribe(() => {
-      this.LogoutButton = this.router.url !== '/login';
+      this.logoutButton = this.router.url !== '/login';
     });
-
   }
   logout(): void {
     this.authService.logout();
