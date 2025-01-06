dfed4435d37dcf0daa42d75914f04bf679cc2e1d
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2025-01-07 01:34:43 +0200

    Add semester info to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+import { SnackBarService } from '../../services/snack-bar.service';
+import { SemesterService } from '../../services/semester.service';
+
+import { Semester } from '../../models/semester.model';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  isNotLogin: boolean = true;
+  semesterInfo: Semester | undefined;
 
+  gettingSemesterData: boolean = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private snackBarService: SnackBarService,
+    private semesterService: SemesterService
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isNotLogin = this.router.url !== '/login';
+      if (this.authService.getToken() && !this.gettingSemesterData) {
+        this.loadCurrentSemester();
+      }
+    });
+  }
+
+  loadCurrentSemester(): void {
+    this.semesterInfo = undefined;
+    this.gettingSemesterData = true;
+    this.semesterService.getCurrentSemester().subscribe({
+      next: (data: Semester) => {
+        this.semesterInfo = data;
+        this.gettingSemesterData = false;
+      },
+      error: (err) => {
+        console.error('Eroare la preluarea semestrului curent:', err);
+        this.snackBarService.show('Eroare la preluarea semestrului curent!', 'error');
+        this.gettingSemesterData = false;
+      },
+    });
+  }
+
+  getCurrentYear(): number {
+    return new Date().getFullYear();
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { SnackBarService } from '../../services/snack-bar.service';
+
 import { UserResponse } from '../../models/user.model';
 
 type UserRole = 'student' | 'professor' | 'admin';
@@ -43,6 +45,7 @@ export class HeaderComponent {
   }
 
   loadUserData(): void {
+    this.userInfo = undefined;
     this.gettingUserData = true;
     this.userService.getUserData().subscribe({
       next: (data: UserResponse) => {
@@ -54,10 +57,12 @@ export class HeaderComponent {
         } else if (data.professor) {
           this.role = 'professor';
         }
+        this.gettingUserData = false;
       },
       error: (err) => {
         console.error('Eroare la preluarea datelor utilizatorului:', err);
         this.snackBarService.show('Eroare la preluarea datelor utilizatorului!', 'error');
+        this.gettingUserData = false;
       },
     });
   }
